6acab7e69e6de3ebe27d8e7bb2da949b108c92c7
paranerd <paranerd.development@gmail.com>
2018-05-30 20:01:40 +0200

    Children working

--- modules/googledrive.py
+++ modules/googledrive.py
@@ -12,17 +12,30 @@ class Google_Drive_Backup():
     project_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     module = 'googledrive'
     config = {}
+    try_count = 0
 
     def __init__(self):
         self.config = self.read_config()
         self.credentials = self.read_credentials()
         self.token = self.read_token()
 
+        if not self.credentials:
+            print("No credentials found")
+            return
+
+        if not self.token:
+            self.enable()
+
     def read_config(self):
         with open(self.project_path + '/config.json', 'r') as f:
             return json.load(f)
 
     def config_get(self, key, default=""):
+        if not self.module in self.config:
+            print("adding")
+            self.config[self.module] = {}
+            self.write_config()
+
         return self.config[self.module][key] if key in self.config[self.module] and self.config[self.module][key] != "" else default
 
     def config_set(self, key, value):
@@ -43,16 +56,12 @@ class Google_Drive_Backup():
         return self.config_get('token')
 
     def execute_request(self, url, headers={}, params={}, method="GET"):
-        # Encode URL
-        url = quote_plus(url)
-
-        # Set Authorization-Header
-        auth_header = {
-            'Authorization': 'Bearer {}'.format(self.token)
-            #'Authorization': "Bearer " + str(self.token)
-        }
-
-        headers.update(auth_header)
+        if "access_token" in self.token:
+            # Set Authorization-Header
+            auth_header = {
+                'Authorization': 'Bearer {}'.format(self.token['access_token'])
+            }
+            headers.update(auth_header)
 
         # Execute request
         if method == 'GET':
@@ -60,89 +69,75 @@ class Google_Drive_Backup():
         else:
             res = requests.post(url, headers=headers, data=params)
 
+        if res.status_code == 401:
+            # Token expired
+            if self.try_count == 0:
+                print("Token expired... Trying to get a new one")
+                self.try_count = self.try_count + 1
+                self.obtain_token()
+            else:
+                raise Exception("Failed to refresh token")
+
         return {'status': res.status_code, 'body': res.json()}
 
-    def children(self, id='root'):
-        res = requests.get(self.GOOGLE_API + "?q='" + str(id) + "' in parents&fields=files(id, name, md5Checksum, mimeType)")
+    def children(self, id='root', indentation=0):
+        #print("Children for " + id)
+        res = self.execute_request(self.GOOGLE_API + "?q='" + id + "'+in+parents&fields=files(id%2Cname%2Cmd5Checksum%2CmimeType)")
+        items = res['body']['files']
+        offset = ' ' * 4 * indentation
 
-        print(res.json())
+        for item in items:
+            if item['mimeType'] == 'application/vnd.google-apps.folder':
+                print('{0}{1} | {2}'.format(offset, item['id'], item['name']))
+                self.children(item['id'], indentation + 1)
+            else:
+                checksum = item['md5Checksum'] if 'md5Checksum' in item else ''
+                print('{0}{1} | {2} -> {3}'.format(offset, item['id'], item['name'], checksum))
 
-    def refresh_token(self):
+    def obtain_token(self, code=""):
         if not self.credentials:
             raise Exception('Could not read credentials')
 
-        if not self.token:
+        if not code and not self.token:
             raise Exception('Could not read token')
-
-        params = {
-          'client_id': self.credentials['client_id'],
-          'client_secret': self.credentials['client_secret'],
-          'refresh_token': self.token['refresh_token'],
-          'grant_type': 'refresh_token'
-        }
-        '''
-        $response = $this->execute_request($this->credentials['token_uri'], $header, http_build_query($params))
-        if($response) {
-            $access_token = $response['body']
-            if ($access_token && array_key_exists('access_token', $access_token)) {
-                $access_token['refresh_token'] = $this->token['refresh_token']
-                $access_token['created'] = time()
-                // Write access token
-                return (file_put_contents($this->token_path, json_encode($access_token)) !== false)
-            }
-        }
-        return false
-        '''
-
-    def create_auth_uri(self):
-        if not self.credentials:
-            raise Exception('Could not read credentials')
-
-        return self.credentials['auth_uri'] + "?response_type=code" + "&redirect_uri=" + quote_plus(self.credentials['redirect_uris'][0]) + "&client_id=" + quote_plus(self.credentials['client_id']) + "&scope=" + quote_plus(self.SCOPES) + "&access_type=" + quote_plus(self.ACCESS) + "&approval_prompt=auto"
-
-    def exchange_code_for_token(self, code):
-        if not self.credentials:
-            raise Exception('Could not read credentials')
 
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
 
         params = {
-            'code'			: code,
             'client_id' 	: self.credentials['client_id'],
             'client_secret'	: self.credentials['client_secret'],
             'redirect_uri'	: self.credentials['redirect_uris'][0],
-            'grant_type'	: 'authorization_code'
         }
+
+        if code:
+            params['grant_type'] = 'authorization_code'
+            params['code'] = code
+        else:
+            params['grant_type'] = 'refresh_token'
+            params['refresh_token'] = self.token['refresh_token']
 
         res = self.execute_request(self.credentials['token_uri'], headers, params, "POST")
 
         if res['status'] == 200:
-            print("Successful: " + str(res['body']))
             self.config_set('token', res['body'])
         else:
-            print("Error getting token: " + res['body'])
+            print("Error getting token: " + str(res['body']))
 
-        #self.credentials['token_uri']
-        #client_id=GOOGLE_CLIENT_ID&client_secret=GOOGLE_CLIENT_SECRET&grant_type=authorization_code&code=AUTHORIZATION_CODE
         print(res)
 
     def enable(self):
-        auth_uri = self.create_auth_uri()
+        # Build auth uri
+        auth_uri = self.credentials['auth_uri'] + "?response_type=code" + "&redirect_uri=" + quote_plus(self.credentials['redirect_uris'][0]) + "&client_id=" + quote_plus(self.credentials['client_id']) + "&scope=" + quote_plus(self.SCOPES) + "&access_type=" + quote_plus(self.ACCESS) + "&approval_prompt=auto"
 
         print("Visit:")
         print(auth_uri)
         print("")
 
-        code = input('Code: ')
+        code = input('Enter code: ')
 
-        self.exchange_code_for_token(code)
+        self.obtain_token(code)
 
 gd = Google_Drive_Backup()
-#gd.enable()
-
 gd.children()
-#auth_url = gd.create_auth_url()
-#print(auth_url)
-#gd.exchange_code_for_token("4/AADCnBemUjKj0sbiQ14dUVMCtdW0y7TeVEnYjY3-KQCR_WqKUZESQSo")
