84e1816638273a856feb3fb97f12c92414bd0256
paranerd <paranerd.development@gmail.com>
2018-07-23 18:23:30 +0000

    Create cache folder

--- backup.py
+++ backup.py
@@ -6,10 +6,14 @@ from modules.googledrive import Google_Drive_Backup
 from modules.googlephotos import Google_Photos_Backup
 
 config_location = os.path.join(os.path.dirname(__file__), "config.json")
+cache_location = os.path.join(os.path.dirname(__file__), "cache")
 
 if not os.path.exists(config_location):
     with open(config_location, 'w') as config:
         config.write('{}')
+
+if not os.path.exists(cache_location):
+    os.makedirs(cache_location)
 
 git = Github_Backup()
 gd = Google_Drive_Backup()
