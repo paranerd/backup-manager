e5bf006ff12465c50431aa0eeb1199de8e81c3cd
paranerd <paranerd.development@gmail.com>
2019-06-21 21:55:36 +0200

    Added method to get entire log

--- modules/log.py
+++ modules/log.py
@@ -3,51 +3,55 @@ import datetime
 from . import util
 
 class Logger():
-    prepared = ""
-    project_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    logs_path = os.path.join(project_path, "log")
-    lock_path = os.path.join(logs_path, "lock")
+	prepared = ""
+	project_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+	logs_path = os.path.join(project_path, "log")
+	lock_path = os.path.join(logs_path, "lock")
 
-    def __init__(self):
-        # Create logs folder
-        util.create_folder(self.logs_path)
+	def __init__(self):
+		# Create logs folder
+		util.create_folder(self.logs_path)
 
-        # Set current log path
-        self.path = self.set_path()
+		# Set current log path
+		self.path = self.set_path()
 
-    ##
-    # Prepare log message without immediate writing
-    #
-    # @param string msg Log message
-    def prepare(self, msg):
-        self.prepared += msg
+	##
+	# Prepare log message without immediate writing
+	#
+	# @param string msg Log message
+	def prepare(self, msg):
+		self.prepared += msg
 
-    ##
-    # Write prepared log message
-    #
-    def flush(self):
-        if self.prepared:
-            self.write(self.prepared)
-            self.prepared = ""
+	##
+	# Write prepared log message
+	#
+	def flush(self):
+		if self.prepared:
+			self.write(self.prepared)
+			self.prepared = ""
 
-    ##
-    # Set current log path
-    #
-    def set_path(self):
-        name = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S')
-        return os.path.join(self.project_path, "log", name)
+	##
+	# Set current log path
+	#
+	def set_path(self):
+		name = datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S')
+		return os.path.join(self.project_path, "log", name)
 
-    ##
-    # Write message to log and print to console
-    #
-    # @param string msg Log message
-    def write(self, msg):
-        # Build message
-        msg = util.get_timestamp(True) + " | " + str(msg)
+	##
+	# Write message to log and print to console
+	#
+	# @param string msg Log message
+	def write(self, msg):
+		# Build message
+		msg = util.get_timestamp(True) + " | " + str(msg)
 
-        # Print message to terminal
-        print(msg)
+		# Print message to terminal
+		print(msg)
 
-        # Write message to file
-        with open(self.path, "a") as logfile:
-            logfile.write(msg + "\n")
+		# Write message to file
+		with open(self.path, "a") as logfile:
+			logfile.write(msg + "\n")
+
+	def get(self):
+		with open(self.path, "r") as file:
+			return file.read()
